7959c82e3be8f8619f0f0160210bbdee064a99ee
Marc Moten <marcmotenii@gmail.com>
2022-11-14 23:39:27 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 // import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
 // const [location, setLocation] = useState('');
 
   return (
     
-    null  );
+    <Navbar/> 
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import './Navbar.css';
+import { Disclosure} from '@headlessui/react'
+
+function Navbar() {
+return (
+    <>
+      <div className="min-h-full">
+        <Disclosure as="nav" className="bg-gray-800">
+          {({ open }) => (
+            <>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 items-center justify-between">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                    <span className='text-white'>WL</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </>
+          )}
+        </Disclosure>
+
+      </div>
+    </>
+  )
+}
+
+export default Navbar;
